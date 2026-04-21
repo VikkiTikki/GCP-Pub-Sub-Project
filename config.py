13c24e72eb199aadb51827eeed0b60d83b0dc434
Victoria Dablah <vdablah@students.kennesaw.edu>
2026-04-21 15:30:13 -0400

    Refactor database configuration in config.py to load environment variables and add password validation

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST", "127.0.0.1"),
-    "user": os.getenv("DB_USER", "user"),
-    "password": os.getenv("DB_PASSWORD", "123456"),
-    "port": int(os.getenv("DB_PORT", 3307))
+    "user": os.getenv("DB_USER", "root"),
+    "password": os.getenv("DB_PASSWORD"),
+    "port": int(os.getenv("DB_PORT", 3306))
 }
+
+if not DB_CONFIG["password"]:
+        raise ValueError("DB_Password is not set.")
