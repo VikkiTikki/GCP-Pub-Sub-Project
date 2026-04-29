720cdf06202072e317a400641c5f4b77c5fe0cb8
Victoria Dablah <vdablah@students.kennesaw.edu>
2026-04-29 15:04:31 -0400

    Fix import path for Database in sub_worker.py to align with new directory structure

--- app/sub_worker.py
+++ app/sub_worker.py
@@ -1,12 +1,13 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from google.cloud import pubsub_v1
 import google.auth
 import json
 from datetime import datetime
 from config import DB_CONFIG
-from db.database import Database
+from storage.database import Database
 
 # ---------------- AUTH ----------------
 credentials, project = google.auth.default()
